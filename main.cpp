8daf972631306d5eb4e03753c1c172b6f0a9c810
denisovamaria <mvdenisova.work@gmail.com>
2023-12-05 19:42:29 +0300

    first try

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
-    
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    int n;
+    in >> n;
+    vector<vector<int>> matrix(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            in >> matrix[i][j];
+        }
+    }
+    vector<int> canon(n, 0);
+
 }
